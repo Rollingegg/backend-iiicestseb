4aa33918b4278bef670dd969791d4809e5a38801
Hui Jiang <814775538@qq.com>
2020-02-23 15:19:50 +0800

    初始化

---
+++ src/test/java/group/iiicestseb/backend/OasisApplicationTests.java
@@ -0,0 +1,13 @@
+package group.iiicestseb.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OasisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
