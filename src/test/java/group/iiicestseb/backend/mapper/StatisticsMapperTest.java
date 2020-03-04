a252cfff5c79cb9c2baa0631e68e991ebe0982c4
Hui Jiang <814775538@qq.com>
2020-03-05 00:39:32 +0800

    修改CSVTest的junit包

--- src/test/java/group/iiicestseb/backend/mapper/StatisticsMapperTest.java
+++ src/test/java/group/iiicestseb/backend/mapper/StatisticsMapperTest.java
@@ -1,7 +1,6 @@
 package group.iiicestseb.backend.mapper;
 
 import group.iiicestseb.backend.entity.Record;
-import org.apache.ibatis.annotations.Select;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,8 +8,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
-
-import static org.junit.Assert.*;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
