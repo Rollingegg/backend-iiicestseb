7999d43dcd14d782bc05216c638b1309e7ecb240
kennyfortune <370774330@qq.com>
2020-03-20 01:23:23 +0800

    增加yml jpa属性，注释全局异常不获取

--- src/main/java/group/iiicestseb/backend/config/OASISExceptionHandler.java
+++ src/main/java/group/iiicestseb/backend/config/OASISExceptionHandler.java
@@ -1,37 +1,37 @@
-package group.iiicestseb.backend.config;
-
-import group.iiicestseb.backend.utils.CSVUtil;
-import group.iiicestseb.backend.vo.Response;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-/**
- * @author jh
- * @date 2020/3/4
- */
-@RestControllerAdvice
-public class OASISExceptionHandler {
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public Response validateErrorException(MethodArgumentNotValidException ex) {
-        final FieldError error = ex.getBindingResult().getFieldError();
-        String errorMessage;
-        if (error != null) {
-            errorMessage = error.getDefaultMessage();
-        } else {
-            errorMessage = ex.getMessage();
-        }
-        return Response.buildFailure(errorMessage);
-    }
-
-//    @ExceptionHandler(CSVUtil.CSVException.class)
-//    public Response CSVErrorException(CSVUtil.CSVException ex){
+//package group.iiicestseb.backend.config;
+//
+//import group.iiicestseb.backend.utils.CSVUtil;
+//import group.iiicestseb.backend.vo.Response;
+//import org.springframework.validation.FieldError;
+//import org.springframework.web.bind.MethodArgumentNotValidException;
+//import org.springframework.web.bind.annotation.ExceptionHandler;
+//import org.springframework.web.bind.annotation.RestControllerAdvice;
+//
+///**
+// * @author jh
+// * @date 2020/3/4
+// */
+//@RestControllerAdvice
+//public class OASISExceptionHandler {
+//    @ExceptionHandler(MethodArgumentNotValidException.class)
+//    public Response validateErrorException(MethodArgumentNotValidException ex) {
+//        final FieldError error = ex.getBindingResult().getFieldError();
+//        String errorMessage;
+//        if (error != null) {
+//            errorMessage = error.getDefaultMessage();
+//        } else {
+//            errorMessage = ex.getMessage();
+//        }
+//        return Response.buildFailure(errorMessage);
+//    }
+//
+////    @ExceptionHandler(CSVUtil.CSVException.class)
+////    public Response CSVErrorException(CSVUtil.CSVException ex){
+////        return Response.buildFailure(ex.getMessage());
+////    }
+//
+//    @ExceptionHandler(IllegalArgumentException.class)
+//    public Response assertErrorException(IllegalArgumentException ex) {
 //        return Response.buildFailure(ex.getMessage());
 //    }
-
-    @ExceptionHandler(IllegalArgumentException.class)
-    public Response assertErrorException(IllegalArgumentException ex) {
-        return Response.buildFailure(ex.getMessage());
-    }
-}
+//}
