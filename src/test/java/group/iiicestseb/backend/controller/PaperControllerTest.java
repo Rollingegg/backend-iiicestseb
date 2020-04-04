364279a5d6b6621af4e8a37bc6f5e3b7b9ba4646
kennyfortune <370774330@qq.com>
2020-04-04 20:56:19 +0800

    修改测试忘加事务注解

--- src/test/java/group/iiicestseb/backend/controller/PaperControllerTest.java
+++ src/test/java/group/iiicestseb/backend/controller/PaperControllerTest.java
@@ -24,6 +24,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
 
 import javax.annotation.Resource;
@@ -34,7 +35,7 @@ import javax.annotation.Resource;
  */
 @SpringBootTest
 @RunWith(SpringRunner.class)
-//@Transactional
+@Transactional
 public class PaperControllerTest {
 
     @Autowired
