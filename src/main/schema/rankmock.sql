ac0f0f40d652e9538705424f05761612e4540ba0
dwxh-mi <171250507@smail.nju.edu.cn>
2020-07-29 01:57:47 +0800

    修改测试数据。

--- src/main/schema/rankmock.sql
+++ src/main/schema/rankmock.sql
@@ -1,9 +1,16 @@
-insert into affiliation (id, name) values (-1,'aff1');
-insert into affiliation (id, name) values (-2,'aff2');
-insert into affiliation (id, name) values (-3,'aff3');
-insert into author (id, name, first_name, last_name, affiliation_id) values (-1,'name1','first1','last1',-1);
-insert into author (id, name, first_name, last_name, affiliation_id) values (-2,'name2','first2','last2',-2);
-insert into author (id, name, first_name, last_name, affiliation_id) values (-3,'name3','first3','last3',-3);
-insert into author_page_rank (author_id, page_rank) values (-1,0.33);
-insert into author_page_rank (author_id, page_rank) values (-2,0.13);
-insert into author_page_rank (author_id, page_rank) values (-3,0.43);
+insert into affiliation (id, name)
+values (-1, 'aff1'),
+       (-2, 'aff2'),
+       (-3, 'aff3');
+insert into author (id, name, first_name, last_name, affiliation_id)
+values (-1, 'name1', 'first1', 'last1', -1),
+       (-2, 'name2', 'first2', 'last2', -2),
+       (-3, 'name3', 'first3', 'last3', -3);
+insert into author_page_rank (author_id, page_rank)
+values (-1, 0.33),
+       (-2, 0.13),
+       (-3, 0.43);
+insert into author_statistics (author_id, h_index, g_index, avg_cite, paper_num, ase_paper_num, icse_paper_num)
+VALUES (-1, 1, 1, 1, 1, 1, 0),
+       (-2, 2, 2, 2, 2, 2, 0),
+       (-3, 3, 3, 3, 2, 1, 1);
